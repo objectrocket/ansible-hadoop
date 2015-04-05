89dc63be78fd7759eee2dd8d95d167e317ede512
Alexandru Anghel <alexandru.anghel@rackspace.co.uk>
2015-04-05 20:25:03 +0100

    Cleanup.

--- hortonworks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export RAX_CREDS_FILE=$(grep rax_credentials_file group_vars/all|cut -d"'" -f2)
-export RAX_REGION=$(grep rax_region group_vars/all|cut -d"'" -f2)
-
-ansible-playbook -i inventory/rax.py hortonworks.yml
