0fd142174b49ffbc981b7460a49997a120d08d56
David Grier <seraphin@gmail.com>
2017-01-26 00:39:47 +0000

    typo

--- playbooks/library/site_facts/sitefacts.py
+++ playbooks/library/site_facts/sitefacts.py
@@ -182,21 +182,21 @@ def spark_defaults_facts(dnmemory):
     spark_defaults=dict()
 
     if (dnmemory > 110):
-        spark_defaults['sparn_executor_memory']="7808m"            
+        spark_defaults['spark_yarn_executor_memory']="7808m"            
         spark_defaults['spark_driver_memory']="7808m"
         spark_defaults['spark_yarn_am_memory']="7808m"
         spark_defaults['spark_yarn_executor_memoryOverhead']="384m"
         spark_defaults['spark_yarn_driver_memoryOverhead']="384m"
         spark_defaults['spark_yarn_am_memoryOverhead']="384m"       
     elif (dnmemory > 57):
-        spark_defaults['sparn_executor_memory']="7808m"            
+        spark_defaults['spark_yarn_executor_memory']="7808m"            
         spark_defaults['spark_driver_memory']="3712m"
         spark_defaults['spark_yarn_am_memory']="3712m"
         spark_defaults['spark_yarn_executor_memoryOverhead']="384m"
         spark_defaults['spark_yarn_driver_memoryOverhead']="384m"    
         spark_defaults['spark_yarn_am_memoryOverhead']="384m"
     else:
-        spark_defaults['sparn_executor_memory']="7808m"            
+        spark_defaults['spark_yarn_executor_memory']="7808m"            
         spark_defaults['spark_driver_memory']="3712m"
         spark_defaults['spark_yarn_am_memory']="3712m"
         spark_defaults['spark_yarn_executor_memoryOverhead']="384m"
