d3cf9bc9158ef6eec9cf53ac5028409288742f01
David Grier <seraphin@gmail.com>
2020-02-21 18:33:50 +0000

    setup python path for virtenv

--- provision_rax.sh
+++ provision_rax.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i inventory/localhost playbooks/provision_rax.yml
+ansible-playbook -i inventory/localhost  --extra-vars="ansible_python_interpreter=$(which python)" playbooks/provision_rax.yml
